dd5f7da9a04bf6413888748efe525490c6af753f
Matthew <m.zadrozny@gmail.com>
2014-02-23 12:20:02 -0500

    Now working

--- laakman/laakman_1_7.py
+++ laakman/laakman_1_7.py
@@ -2,46 +2,40 @@
 '''Write an algorithm such that if an element in an MxN matrix is 0, 
 its entire row and column is set to 0.'''
 
-a = [[1, 0], [2, 3]] #--> [[0, 0], [2, 0]]
 
-b = [[1, 2, 0], [4, 0, 6], [7, 8, 9]] #--> [[0, 0, 0], [0, 0, 0], [7, 0, 0]]
-
-def convert_column(m, r, c):
-	for i, row in enumerate(m[r:]):
-		m[i][c] = 0 
-	print m
-	return m 
-
-def convert(matrix):
+def convert_column(matrix, column_number):
 	for i, row in enumerate(matrix):
-		for x, element in enumerate(row): 
-			if element == 0: 
-				matrix[i] = [0]*len(row) #set row to 0
-				convert_column(matrix, i, x) #set column to 0
-	return matrix
+		matrix[i][column_number] = 0 
 
-#print convert(a)
+
+def convert_row(matrix, row_number):
+	matrix[row_number] = [0]*len(matrix[row_number])
+
 
 def convert(matrix):
-	width  = len(matrix[0])
-	height = len(a)
-
+	columns = []
+	rows    = []
 	for y, row in enumerate(matrix):
 		for x, column_element in enumerate(row):
-			if column_element == 0:
-				if y == 0:
-					matrix[y] = [0]*width
-					for row in matrix[y+1:]:
-						if matrix[y][x] == 0:
-							row[x] = 0
-				elif y != 0 and matrix[x-1][y-1] != 0:
-					print "yes", x, y, matrix, matrix[x-1][y-1]
-					matrix[y] = [0]*width			
+			if matrix[y][x] == 0:
+				columns.append(x)
+				rows.append(y)
+
+	for column in columns:
+		convert_column(matrix, column)
+
+	for row in rows: 
+		convert_row(matrix, row)
+
 	return matrix
 
-print convert(b)
 
 
+a = [[1, 0], [2, 3]] 
+
+b = [[1, 2, 0], [4, 0, 6], [7, 8, 9]] 
 
 
+assert convert(a) == [[0, 0], [2, 0]]
 
+assert convert(b) == [[0, 0, 0], [0, 0, 0], [7, 0, 0]]
