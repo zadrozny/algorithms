8dfbcdb12f7fc725fef5097e0d108881012c1608
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-10-22 11:52:27 -0400

    Using timeit on all three functions

--- find_list_duplicates.py
+++ find_list_duplicates.py
@@ -8,10 +8,11 @@ Write a test for this.
 
 def find_duplicates_1(lst):
 	duplicates = []
-	for element in set(lst):
+	for element in set(lst): 
 		if lst.count(element) > 1:
 			duplicates.append(element)
 	return duplicates
+
 
 
 #Rewritten as a list comprehension
@@ -19,19 +20,36 @@ def find_duplicates_2(lst):
 	return [element for element in set(lst) if lst.count(element) > 1]
 
 
-#From http://stackoverflow.com/a/9835819/1366410
+
+#Using collections, a la http://stackoverflow.com/a/9835819/1366410
 import collections
 def find_duplicates_3(lst):
 	return [x for x, y in collections.Counter(lst).items() if y > 1]
 
 
-tests = {
-(1, 2, 3, 4, 5, 5, 6, 6): [5, 6],
-(1, 2, 3, 4, 5, 6, 5): [5]
-}
+
+if __name__ == '__main__':
+
+	tests = {
+	(1, 2, 3, 4, 5, 5, 6, 6): [5, 6],
+	(1, 2, 3, 4, 5, 6, 5): [5]
+	}
 
 
-for test in tests:
-	assert find_duplicates_1(test) == tests[test]
-	assert find_duplicates_2(test) == tests[test]
-	assert find_duplicates_3(test) == tests[test]
+	for test in tests:
+		assert find_duplicates_1(test) == tests[test]
+		assert find_duplicates_2(test) == tests[test]
+		assert find_duplicates_3(test) == tests[test]
+
+
+	import timeit
+
+	print(timeit.timeit("for x in range(10, 11): find_duplicates_1(range(x))", 
+		setup="from __main__ import find_duplicates_1"))
+
+	print(timeit.timeit("for x in range(10, 11): find_duplicates_2(range(x))", 
+		setup="from __main__ import find_duplicates_2"))
+
+	print(timeit.timeit("for x in range(10, 11): find_duplicates_3(range(x))", 
+		setup="from __main__ import find_duplicates_3"))
+
