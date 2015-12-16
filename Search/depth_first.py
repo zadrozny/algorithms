9c0ab9983d5eceb04a5478eb38196f88d460c84a
Matthew <m.zadrozny@gmail.com>
2015-12-15 22:16:11 -0500

    Indents

--- Search/depth_first.py
+++ Search/depth_first.py
@@ -6,17 +6,17 @@ import os
 
 
 def depth_first(children=None, stack=[]):
-	'''
-	Perform a depth-first search / walkthrough on a directory (containing only directories, no files). The first 
-	'''
+    '''
+    Perform a depth-first search / walkthrough on a directory (containing only directories, no files). The first 
+    '''
 
-	children = [children + '/' + c for c in os.listdir(children)]
+    children = [children + '/' + c for c in os.listdir(children)]
 
-	for c in children:
-		stack.append(c)
-		depth_first(children=c, stack=stack)
+    for c in children:
+        stack.append(c)
+        depth_first(children=c, stack=stack)
 
-	return stack
+    return stack
 
 
 
